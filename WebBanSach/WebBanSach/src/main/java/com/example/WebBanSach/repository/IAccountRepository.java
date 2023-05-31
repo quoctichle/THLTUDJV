604ef680ff4b850b3254d1ad29589f4d533b60c0
quoctichle <quoctichle@gmail.com>
2023-05-31 09:54:07 +0700

    sai file

--- WebBanSach/WebBanSach/src/main/java/com/example/WebBanSach/repository/IAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.WebBanSach.repository;
-
-import com.example.WebBanSach.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAccountRepository extends JpaRepository<Account,Long> {
-
-}
